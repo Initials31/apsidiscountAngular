339ef02ce3ff39c8f8cb6a67f2cb696529e5411e
Devv972 <henryd.pro@gmail.com>
2019-07-19 15:48:42 +0200

    fixation des bug

--- src/app/service/categorie.service.ts
+++ src/app/service/categorie.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Categorie } from '../categorie/categorie';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Article } from '../article/article';
 
 @Injectable({
   providedIn: 'root'
@@ -21,4 +22,9 @@ export class CategorieService {
   getAllCategoriesJson(): Observable<Categorie[]> {
     return this.http.get<Categorie[]>(`${this.baseURL}/categorie`, this.httpOptions);
   }
+
+  getArticleByCategorieId (id: number): Observable<Article> {
+    return this.http.get<Article>(`${this.baseURL}/categorie/{id}`, this.httpOptions);
+  }
+  
 }
